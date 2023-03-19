d9fb5a86b4235328e2ea8a641cb577d9745e442b
namnpse <nam444479@gmail.com>
2023-03-19 19:30:48 +0700

    added hero model, constant

---
+++ app/src/main/java/com/namnp/heroes/domain/model/Hero.kt
@@ -0,0 +1,20 @@
+package com.namnp.heroes.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.namnp.heroes.util.Constants.HERO_DATABASE_TABLE
+
+@Entity(tableName = HERO_DATABASE_TABLE)
+data class Hero(
+    @PrimaryKey(autoGenerate = false)
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>
+)
--- app/src/main/java/com/namnp/heroes/util/Constants.kt
+++ app/src/main/java/com/namnp/heroes/util/Constants.kt
@@ -3,5 +3,7 @@ package com.namnp.heroes.util
 object Constants {
 
     const val DETAILS_HERO_KEY = "hero_id"
+    const val HERO_DATABASE_TABLE = "hero_table"
+    const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
 
 }
