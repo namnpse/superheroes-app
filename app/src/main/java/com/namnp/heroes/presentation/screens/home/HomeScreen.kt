d5c14393d066d0851ef160e5830b82454bbe9bd3
namnpse <nam444479@gmail.com>
2023-03-20 02:37:02 +0700

    built app bar

--- app/src/main/java/com/namnp/heroes/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/namnp/heroes/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.namnp.heroes.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {}
 }
---
+++ app/src/main/java/com/namnp/heroes/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,47 @@
+package com.namnp.heroes.presentation.screens.home
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.namnp.heroes.ui.theme.*
+import com.namnp.heroes.R
+
+@Composable
+fun HomeTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Explore",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
+
+@Composable
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+fun  HomeTopBarDarkPreview() {
+    HomeTopBar {
+
+    }
+}
+
