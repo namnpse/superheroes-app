3fee81e721d7c44d312c11a705e86e0da32538df
namnpse <nam444479@gmail.com>
2023-03-19 22:05:51 +0700

    create database converter

--- app/src/main/java/com/namnp/heroes/data/local/HeroDatabase.kt
+++ app/src/main/java/com/namnp/heroes/data/local/HeroDatabase.kt
@@ -1,11 +1,13 @@
 package com.namnp.heroes.data.local
 
 import androidx.room.Database
+import androidx.room.TypeConverters
 import com.namnp.heroes.data.local.dao.HeroDao
 import com.namnp.heroes.data.local.dao.HeroRemoteKeyDao
 import com.namnp.heroes.domain.model.Hero
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKeyDao::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase {
 
     abstract fun heroDao() : HeroDao
