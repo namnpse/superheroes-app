fd8fca46d893010478b04e65951de496a09df432
namnpse <nam444479@gmail.com>
2023-03-20 02:50:00 +0700

    setup network module di

---
+++ app/src/main/java/com/namnp/heroes/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.namnp.heroes.di
+
+import androidx.paging.ExperimentalPagingApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.namnp.heroes.data.remote.HeroApi
+import com.namnp.heroes.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalPagingApi
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHeroApi(retrofit: Retrofit): HeroApi {
+        return retrofit.create(HeroApi::class.java)
+    }
+}
