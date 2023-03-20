ce54a96293e6f8ef820d81207c2073ebce14ad93
namnpse <nam444479@gmail.com>
2023-03-20 21:20:35 +0700

    created search widget

---
+++ app/src/main/java/com/namnp/heroes/presentation/screens/search/SearchScreen.kt
@@ -0,0 +1,40 @@
+package com.namnp.heroes.presentation.screens.search
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+import com.namnp.heroes.presentation.common.ListContent
+
+@ExperimentalCoilApi
+@Composable
+fun SearchScreen(
+    navController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
+    val searchQuery by searchViewModel.searchQuery
+    val heroes = searchViewModel.searchedHeroes.collectAsLazyPagingItems()
+
+    Scaffold(
+        topBar = {
+            SearchTopBar(
+                text = searchQuery,
+                onTextChange = {
+                    searchViewModel.updateSearchQuery(query = it)
+                },
+                onSearchClicked = {
+                    searchViewModel.searchHeroes(query = it)
+                },
+                onCloseClicked = {
+                    navController.popBackStack()
+                }
+            )
+        },
+        content = {
+            ListContent(heroes = heroes, navController = navController)
+        }
+    )
+}
