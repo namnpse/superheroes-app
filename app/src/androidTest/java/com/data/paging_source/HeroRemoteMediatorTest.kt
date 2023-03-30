8ff95c20e2318edf5ca7a219663b47a892990150
namnpse <nam444479@gmail.com>
2023-03-30 21:54:49 +0700

    setup instrumentation test for hero remote mediator api

---
+++ app/src/androidTest/java/com/data/paging_source/HeroRemoteMediatorTest.kt
@@ -0,0 +1,106 @@
+package com.data.paging_source
+
+import androidx.paging.*
+import androidx.paging.RemoteMediator.*
+import androidx.test.core.app.ApplicationProvider
+import com.data.remote.FakeRemoteMediatorApi
+import com.namnp.heroes.data.local.HeroDatabase
+import com.namnp.heroes.data.paging_source.HeroRemoteMediator
+import com.namnp.heroes.domain.model.Hero
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class HeroRemoteMediatorTest {
+
+    private lateinit var heroApi: FakeRemoteMediatorApi
+    private lateinit var heroDatabase: HeroDatabase
+
+    @Before
+    fun setup() {
+        heroApi = FakeRemoteMediatorApi()
+        heroDatabase = HeroDatabase.create(
+            context = ApplicationProvider.getApplicationContext(),
+            useInMemory = true
+        )
+    }
+
+    @After
+    fun cleanup() {
+        heroDatabase.clearAllTables()
+    }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsSuccessResultWhenMoreDataIsPresent() =
+        runBlocking {
+            val remoteMediator = HeroRemoteMediator(
+                heroApi = heroApi,
+                heroDatabase = heroDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Success)
+            assertFalse((result as MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadSuccessAndEndOfPaginationTrueWhenNoMoreData() =
+        runBlocking {
+            heroApi.clearData()
+            val remoteMediator = HeroRemoteMediator(
+                heroApi = heroApi,
+                heroDatabase = heroDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Success)
+            assertTrue((result as MediatorResult.Success).endOfPaginationReached)
+        }
+
+    @ExperimentalPagingApi
+    @Test
+    fun refreshLoadReturnsErrorResultWhenErrorOccurs() =
+        runBlocking {
+            heroApi.fakeException()
+            val remoteMediator = HeroRemoteMediator(
+                heroApi = heroApi,
+                heroDatabase = heroDatabase
+            )
+            val pagingState = PagingState<Int, Hero>(
+                pages = listOf(),
+                anchorPosition = null,
+                config = PagingConfig(pageSize = 3),
+                leadingPlaceholderCount = 0
+            )
+            val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+            assertTrue(result is MediatorResult.Error)
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/namnp/heroes/data/local/HeroDatabase.kt
+++ app/src/main/java/com/namnp/heroes/data/local/HeroDatabase.kt
@@ -1,6 +1,8 @@
 package com.namnp.heroes.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.namnp.heroes.data.local.dao.HeroDao
@@ -11,6 +13,19 @@ import com.namnp.heroes.domain.model.HeroRemoteKeys
 @Database(entities = [Hero::class, HeroRemoteKeys::class], version = 1)
 @TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase : RoomDatabase() {
+
+    companion object {
+        fun create(context: Context, useInMemory: Boolean): HeroDatabase {
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, HeroDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, HeroDatabase::class.java, "test_database.db")
+            }
+            return databaseBuilder
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
 
     abstract fun heroDao() : HeroDao
     abstract fun heroRemoteKeysDao() : HeroRemoteKeysDao
