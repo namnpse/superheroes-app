0e5db51f69a03edeb13674f6ae8a41787a8e6463
namnpse <nam444479@gmail.com>
2023-04-12 23:26:50 +0700

    show banner in home screen

--- app/src/main/java/com/namnp/heroes/presentation/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/namnp/heroes/presentation/screens/home/HomeViewModel.kt
@@ -1,8 +1,12 @@
 package com.namnp.heroes.presentation.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.namnp.heroes.domain.model.Hero
 import com.namnp.heroes.domain.use_cases.UseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +15,17 @@ class HomeViewModel @Inject constructor(
 ): ViewModel() {
     val getAllHeroes = useCases.getAllHeroesUseCase()
     val getMarvelHeroes = useCases.getMarvelHeroesUseCase()
+    val getBannersUseCase = useCases.getBannersUseCase
+    private val _banners = MutableStateFlow<List<Hero>>(emptyList())
+    val banners = _banners
+
+    init {
+        getBanners()
+    }
+
+    fun getBanners() {
+        viewModelScope.launch {
+            _banners.value = getBannersUseCase()
+        }
+    }
 }
