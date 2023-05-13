b1af4f33d445ea97d00c2a61bd9930cf6b8b760e
namnpse <nam444479@gmail.com>
2023-05-13 12:35:18 +0700

    switch navigation to viewpager for better UX performance, avoid reloading screens too much

--- app/src/main/java/com/namnp/heroes/presentation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/namnp/heroes/presentation/components/BottomNavigationBar.kt
@@ -3,24 +3,29 @@ package com.namnp.heroes.presentation.components
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
 import com.namnp.heroes.R
 import com.namnp.heroes.presentation.screens.home.NavigationItem
 import com.namnp.heroes.ui.theme.Purple500_Black
 import com.namnp.heroes.ui.theme.White_Purple500
 import com.namnp.heroes.ui.theme.fonts
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun BottomNavigationBar(navController: NavController) {
-    val items = listOf(
-        NavigationItem.Home,
-        NavigationItem.Favorite,
-        NavigationItem.Profile,
-    )
+fun BottomNavigationBar(
+//    navController: NavController,
+    pagerState: PagerState,
+    items: List<NavigationItem>,
+) {
+    val coroutineScope = rememberCoroutineScope()
     val bgColor = MaterialTheme.colors.Purple500_Black
     val contentColor = MaterialTheme.colors.White_Purple500
 
@@ -28,9 +33,9 @@ fun BottomNavigationBar(navController: NavController) {
         backgroundColor = bgColor,
         contentColor = Color.White
     ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
+//        val navBackStackEntry by navController.currentBackStackEntryAsState()
+//        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEachIndexed { index, item ->
             BottomNavigationItem(
                 icon = {
                     if (item.title == "Home")
@@ -44,22 +49,26 @@ fun BottomNavigationBar(navController: NavController) {
                 selectedContentColor = contentColor,
                 unselectedContentColor = contentColor.copy(0.6f),
                 alwaysShowLabel = true,
-                selected = currentRoute == item.route,
+//                selected = currentRoute == item.route,
+                selected = pagerState.currentPage == index,
                 onClick = {
-                    navController.navigate(item.route) {
-                        // Pop up to the start destination of the graph to
-                        // avoid building up a large stack of destinations
-                        // on the back stack as users select items
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
-                            }
-                        }
-                        // Avoid multiple copies of the same destination when
-                        // reselecting the same item
-                        launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
-                        restoreState = true
+//                    navController.navigate(item.route) {
+//                        // Pop up to the start destination of the graph to
+//                        // avoid building up a large stack of destinations
+//                        // on the back stack as users select items
+//                        navController.graph.startDestinationRoute?.let { route ->
+//                            popUpTo(route) {
+//                                saveState = true
+//                            }
+//                        }
+//                        // Avoid multiple copies of the same destination when
+//                        // reselecting the same item
+//                        launchSingleTop = true
+//                        // Restore state when reselecting a previously selected item
+//                        restoreState = true
+//                    }
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
                     }
                 }
             )
--- app/src/main/java/com/namnp/heroes/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/namnp/heroes/presentation/screens/main/MainScreen.kt
@@ -1,17 +1,23 @@
 package com.namnp.heroes.presentation.screens.main
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
 import com.namnp.heroes.R
 import com.namnp.heroes.presentation.components.BottomNavigationBar
 import com.namnp.heroes.presentation.screens.favorite.FavoriteScreen
@@ -20,19 +26,37 @@ import com.namnp.heroes.presentation.screens.home.NavigationItem
 import com.namnp.heroes.presentation.screens.profile.ProfileScreen
 import com.namnp.heroes.ui.theme.ThemeState
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MainScreen(
     appNavController: NavHostController,
     themeState: MutableState<ThemeState>,
 ) {
-    val navBottomNavigationController = rememberNavController()
+//    val navBottomNavigationController = rememberNavController()
+    val mainScreens = listOf(
+        NavigationItem.Home,
+        NavigationItem.Favorite,
+        NavigationItem.Profile,
+    )
+    val pagerState = rememberPagerState()
     Scaffold(
-        bottomBar = { BottomNavigationBar(navBottomNavigationController) },
+        bottomBar = {
+            BottomNavigationBar(
+//            navBottomNavigationController,
+                pagerState,
+                mainScreens,
+            )
+        },
         content = { padding -> // We have to pass the scaffold inner padding to our content. That's why we use Box.
             Box(modifier = Modifier.padding(padding)) {
-                Navigation(
+//                Navigation(
+//                    appNavController = appNavController,
+//                    navBottomNavigationController = navBottomNavigationController,
+//                    themeState = themeState
+//                )
+                ViewPagerScreen(
+                    pagerState = pagerState,
                     appNavController = appNavController,
-                    navBottomNavigationController = navBottomNavigationController,
                     themeState = themeState
                 )
             }
@@ -41,22 +65,47 @@ fun MainScreen(
     )
 }
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun Navigation(
+//fun BottomNavGraph(
+fun ViewPagerScreen(
+    pagerState: PagerState,
     appNavController: NavHostController,
-    navBottomNavigationController: NavHostController,
     themeState: MutableState<ThemeState>,
 ) {
-    NavHost(navBottomNavigationController, startDestination = NavigationItem.Home.route) {
-        composable(NavigationItem.Home.route) {
-            HomeScreen(navController = appNavController)
-        }
-        composable(NavigationItem.Favorite.route) {
-            FavoriteScreen()
-        }
-        composable(NavigationItem.Profile.route) {
-            ProfileScreen(appNavController, themeState = themeState)
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        HorizontalPager(
+            count = 3,
+            state = pagerState,
+        ) { page ->
+            when (page) {
+                0 -> HomeScreen(navController = appNavController)
+                1 -> FavoriteScreen()
+                2 -> ProfileScreen(appNavController, themeState = themeState)
+            }
         }
     }
 }
+
+//@OptIn(ExperimentalMaterialApi::class)
+//@Composable
+//fun Navigation(
+//    appNavController: NavHostController,
+//    navBottomNavigationController: NavHostController,
+//    themeState: MutableState<ThemeState>,
+//) {
+//    NavHost(navBottomNavigationController, startDestination = NavigationItem.Home.route) {
+//        composable(NavigationItem.Home.route) {
+//            HomeScreen(navController = appNavController)
+//        }
+//        composable(NavigationItem.Favorite.route) {
+//            FavoriteScreen()
+//        }
+//        composable(NavigationItem.Profile.route) {
+//            ProfileScreen(appNavController, themeState = themeState)
+//        }
+//    }
+//}
